9d6c0629667bdc77b77697fb79603d0d5d07d3c4
Adrian Nowakowski <anowakowski@objectivity.co.uk>
2017-10-30 10:08:18 +0100

    remove unnecessary pages. Added timer page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 @Component({
   templateUrl: 'app.html'
@@ -12,18 +10,12 @@ import { ListPage } from '../pages/list/list';
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
-  rootPage: any = HomePage;
+  rootPage: any = null;
 
   pages: Array<{title: string, component: any}>;
 
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
-
-    // used for an example of ngFor and navigation
-    this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
-    ];
 
   }
 
@@ -40,5 +32,10 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+  }
+
+  goToStartGame(){
+    const me = this;
+    me.nav.push()
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,15 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { TimerPage } from '../pages/pages';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage,
-    ListPage
+    TimerPage
   ],
   imports: [
     BrowserModule,
@@ -22,8 +20,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage,
-    ListPage
+    TimerPage  
   ],
   providers: [
     StatusBar,
