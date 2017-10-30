a0191fcdfbf52da2d8abd231eac59855528e66d9
Adrian Nowakowski <anowakowski@objectivity.co.uk>
2017-10-30 10:11:40 +0100

    remove ionic decorator

--- src/pages/timer/timer.ts
+++ src/pages/timer/timer.ts
@@ -1,14 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the TimerPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
-@IonicPage()
 @Component({
   selector: 'page-timer',
   templateUrl: 'timer.html',
