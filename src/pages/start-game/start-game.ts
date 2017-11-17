ebe1fd525768413fe711988a01ebbaffc83fc7a2
anowakowski <nowakowski.add@gmail.com>
2017-11-17 21:27:48 +0100

    next for start-game view and logic

--- src/pages/start-game/start-game.ts
+++ src/pages/start-game/start-game.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { GameTypePage } from '../pages';
+import { retry } from 'rxjs/operator/retry';
 
 @Component({
   selector: 'page-start-game',
@@ -10,6 +11,7 @@ export class StartGamePage {
   randomNumber1: number = 0;
   randomNumber2: number = 0;
   correctResult: number = 0;
+  correctResultPosition: number = 1;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     console.log(navParams.data.parameterData);
@@ -25,15 +27,24 @@ export class StartGamePage {
     me.setRandomNumber();
 
     me.correctResult = me.randomNumber1 + me.randomNumber2;
+    me.correctResultPosition = me.getRandomPosition();
   }
 
   private setRandomNumber():void {
     const me = this;
-    me.randomNumber1 = me.getRandomNumber();
-    me.randomNumber2 = me.getRandomNumber();
+    me.randomNumber1 = me.getRandomNumberToMath();
+    me.randomNumber2 = me.getRandomNumberToMath();
   }
 
-  private getRandomNumber():number{
-    return Math.floor(Math.random() * 100) + 1;
+  private getRandomNumberToMath():number{
+    return this.getRandomNumber(100);
+  }
+
+  private getRandomPosition():number{
+    return this.getRandomNumber(4);
+  }
+
+  private getRandomNumber(range:number):number{
+    return Math.floor(Math.random() * range) + 1;
   }
 }
