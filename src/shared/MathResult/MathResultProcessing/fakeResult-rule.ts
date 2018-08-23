3c971f8d552273a4a0f65fbcd4a6cfe5a5a3d080
anowakowski <nowakowski.add@gmail.com>
2018-08-23 10:50:08 +0200

    prepare fakeResult rule. Create result position rule

--- src/shared/MathResult/MathResultProcessing/fakeResult-rule.ts
+++ src/shared/MathResult/MathResultProcessing/fakeResult-rule.ts
@@ -5,8 +5,18 @@ import * as _ from 'lodash';
 export class FakeResultRule extends MathResultBase {
     runMathResult(configuration : MathResultConfigurationModel, mathResults : Array<MathResultModel>)   : void {
         let correctResult:MathResultModel = _.filter(mathResults, mr => !mr.isFakeResult);
+        let mathResultModelForFakeRundom1:MathResultModel = new MathResultModel();
+        let mathResultModelForFakeRundom2:MathResultModel = new MathResultModel();
 
-        let fakeRandomNumber1 = this.prepareRandomNumber(correctResult.result, configuration.gameLevel)
-   
+        let fakeRandomNumber1:number = this.prepareRandomNumber(correctResult.result, configuration.gameLevel);
+        let fakeRandomNumber2:number = this.prepareRandomNumber(correctResult.result, fakeRandomNumber1);
+
+        mathResultModelForFakeRundom1.isFakeResult = true;
+        mathResultModelForFakeRundom1.result = fakeRandomNumber1;
+        mathResultModelForFakeRundom2.isFakeResult = true;
+        mathResultModelForFakeRundom2.result = fakeRandomNumber2;
+
+        mathResults.push(mathResultModelForFakeRundom1);
+        mathResults.push(mathResultModelForFakeRundom2);
     }
 }
